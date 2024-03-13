3bf7cc4b12b661ddb795bc9acae7aa25e76074c0
MD. Shimul <mdshemul480@gmail.com>
2024-03-13 21:54:31 +0600

    adding router system into frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './Components/LoginPage/LoginPage';
+import Dashboard from './Components/Dashboard/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
